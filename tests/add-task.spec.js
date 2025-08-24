0778e921954c61fe000a465854586ceaf3b63407
DennisMaya <MayaDennis101@gmail.com>
2025-08-24 08:32:48 -0700

    added new test to verify that input field is cleared after adding a todo

--- tests/add-task.spec.js
+++ tests/add-task.spec.js
@@ -1,8 +1,19 @@
 import {test, expect} from '@playwright/test';
 
-test('should add a new todo', async({page}) => {
-    await page.goto('https://todomvc.com/examples/react/dist/');
-    await page.getByTestId('text-input').fill('Buy milk');
-    await page.keyboard.press('Enter');
-    await expect(page.locator('.todo-list li')).toHaveText(['Buy milk']);
+test.describe('TodoMVC - Add Todo Workflow', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://todomvc.com/examples/react/dist/');
+    });
+
+    test('should add a new todo', async({page}) => {
+        await page.getByTestId('text-input').fill('Buy milk');
+        await page.keyboard.press('Enter');
+        await expect(page.locator('.todo-list li')).toHaveText(['Buy milk']);
+    });
+
+    test('should clear input after adding todo', async ({page}) => {
+        await page.getByTestId('text-input').fill('Buy eggs');
+        await page.keyboard.press('Enter');
+        await expect(page.getByTestId('text-input')).toHaveValue(''); 
+    });
 });
