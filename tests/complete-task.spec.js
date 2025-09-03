818c2813db138b8a860effabbeb3a04569f51c80
DennisMaya <MayaDennis101@gmail.com>
2025-09-03 14:50:29 -0700

    refactor: updated the complete-task.spec.js file to include a describe and beforeEach method

--- tests/complete-task.spec.js
+++ tests/complete-task.spec.js
@@ -1,16 +1,20 @@
 import {test, expect} from '@playwright/test';
 
-test('should complete a task', async({page}) => {
-    await page.goto('https://todomvc.com/examples/react/dist/');
+test.describe('TodoMVC - Complete Todo Workdlow', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://todomvc.com/examples/react/dist/');
+        //Add a task
+        await page.getByTestId('text-input').fill('Buy milk');
+        await page.keyboard.press('Enter');
+    });
+    test('should complete a task', async({page}) => {
+        //Mark it as completed
+        const checkbox = page.getByTestId('todo-item-toggle');
+        await checkbox.check()
 
-    //Add a task
-    await page.getByTestId('text-input').fill('Buy milk');
-    await page.keyboard.press('Enter');
-
-    //Mark it as completed
-    const checkbox = page.getByTestId('todo-item-toggle');
-    await checkbox.check()
-
-    //Assert it is checked
-    await expect(checkbox).toBeChecked();
+        //Assert it is checked
+        await expect(checkbox).toBeChecked();
+    });
+    
 });
+
