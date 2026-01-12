ca642175f3c4e9c85b24e1a6972a98b48db7c40a
DennisMaya <MayaDennis101@gmail.com>
2026-01-12 13:31:14 -0800

    test: added a test case for deleteing all todo items and asserting an empty list

--- tests/delete-task.spec.js
+++ tests/delete-task.spec.js
@@ -1,18 +1,47 @@
 import {test, expect} from '@playwright/test';
 
-test('should delete a task', async({page}) => {
-    await page.goto('https://todomvc.com/examples/react/dist/');
+test.describe('TodoMVC - Delete Todo Workflow', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://todomvc.com/examples/react/dist/');
+    });
 
-    //Add a task
-    await page.getByTestId('text-input').fill('Buy milk');
-    await page.keyboard.press('Enter');
+    test('should delete a task', async({page}) => {
+        //Add a task
+        await page.getByTestId('text-input').fill('Buy milk');
+        await page.keyboard.press('Enter');
 
-    //Mark it as completed
-    await page.getByTestId('todo-item-toggle').check();
+        //Mark it as completed
+        await page.getByTestId('todo-item-toggle').check();
 
-    //Delete the task
-    await page.getByTestId('todo-item-button').click();
+        //Delete the task
+        await page.getByTestId('todo-item-button').click();
 
-    //Assert the task is deleted
-    await expect(page.locator('todo-list')).toHaveCount(0);
+        //Assert the task is deleted
+        await expect(page.locator('todo-list')).toHaveCount(0);
+    });
+
+    test('should display an emtpy list after all tasks are deleted', async({page}) => {
+        const tasks = ['Buy milk', 'Buy eggs', 'Buy butter'];
+
+        //Add a tasks
+        for (const task of tasks) {
+            await page.locator('.new-todo').fill(task);
+            await page.keyboard.press('Enter');
+        }
+
+
+        // Delete all tasks one by one
+        const items = page.locator('.todo-list li');
+        const count = await items.count();
+
+        for (let i = 0; i < count; i++) {
+            await items.nth(0).hover();  
+            await items.nth(0).locator('.destroy').click();
+        }
+
+        // Assert list is empty
+        await expect(page.locator('.todo-list li')).toHaveCount(0);
+
+    });
+
 });
