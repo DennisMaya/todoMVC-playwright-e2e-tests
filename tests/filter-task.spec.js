586677231bc631a84fba5d1d89485812f97c9d06
DennisMaya <MayaDennis101@gmail.com>
2026-02-02 15:26:53 -0800

    test: added test to verify that only incompleted tasks are visible whilte active filter is on

--- tests/filter-task.spec.js
+++ tests/filter-task.spec.js
@@ -1,27 +1,33 @@
 import {test, expect} from '@playwright/test';
 
-test('should display active tasks', async({page}) => {
-    await page.goto('https://todomvc.com/examples/react/dist/');
+test.describe('TodoMVC - Complete User Journey Workflow', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://todomvc.com/examples/react/dist/');
+        
+        //Add a task
+        await page.getByTestId('text-input').fill('Buy milk');
+        await page.keyboard.press('Enter');
 
-    //Add a task
-    await page.getByTestId('text-input').fill('Buy milk');
-    await page.keyboard.press('Enter');
+        //Add another task
+        await page.getByTestId('text-input').fill('Buy eggs');
+        await page.keyboard.press('Enter');
 
-    //Add another task
-    await page.getByTestId('text-input').fill('Buy eggs');
-    await page.keyboard.press('Enter');
+        //Mark "Buy milk" task as completed
+        await page
+            .getByRole('listitem')
+            .filter({hasText: 'Buy milk'})
+            .getByTestId('todo-item-toggle')
+            .check();
 
-    //Mark "Buy milk" task as completed
-    await page
-        .getByRole('listitem')
-        .filter({hasText: 'Buy milk'})
-        .getByTestId('todo-item-toggle')
-        .check();
+    });
 
-    //Click the Active filter
-    await page.getByTestId('footer-navigation').getByText('Active').click();
+    test('should display active tasks', async({page}) => {
+        //Click the Active filter
+        await page.getByTestId('footer-navigation').getByText('Active').click();
 
-    //Assert only "Buy eggs" is visible
-    await expect(page.getByTestId('todo-list')).toHaveCount(1);
-    await expect(page.getByTestId('todo-list')).toHaveText(['Buy eggs']);
+        //Assert only "Buy eggs" is visible
+        const list = await page.locator('.todo-list li');
+        await expect(list).toHaveCount(1);
+        await expect(list).toHaveText('Buy eggs');
+    });
 });
